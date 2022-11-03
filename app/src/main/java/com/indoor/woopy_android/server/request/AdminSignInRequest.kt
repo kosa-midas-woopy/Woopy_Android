ee1dc6a47fd5e44eedfd9bdf023f48eec06152d8
KimHaNeul <s2025@e-mirim.hs.kr>
2022-11-04 06:13:26 +0900

    Feat : 일반 사용자 로그인 기능 추가

---
+++ app/src/main/java/com/indoor/woopy_android/server/request/AdminSignInRequest.kt
@@ -0,0 +1,6 @@
+package com.indoor.woopy_android.server.request
+
+data class AdminSignInRequest (
+    var email : String,
+    var password : String
+)
--- app/src/main/java/com/indoor/woopy_android/view/user/UserLoginActivity.kt
+++ app/src/main/java/com/indoor/woopy_android/view/user/UserLoginActivity.kt
@@ -6,9 +6,12 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.indoor.woopy_android.R
 import com.indoor.woopy_android.databinding.ActivityUserLoginBinding
+import com.indoor.woopy_android.view.user.viewmodel.UserLoginViewModel
 
 class UserLoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityUserLoginBinding
+    private lateinit var loginViewModel : UserLoginViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(
@@ -16,13 +19,12 @@ class UserLoginActivity : AppCompatActivity() {
             R.layout.activity_user_login
         )
 
+        observeViewModel()
+
         binding.backBtn.setOnClickListener {
             super.onBackPressed()
         }
 
-        binding.btnUserLogin.setOnClickListener {
-
-        }
 
         binding.tvUserSignUp.setOnClickListener {
             val intent = Intent(this@UserLoginActivity, UserSignupUserActivity::class.java)
@@ -31,5 +33,12 @@ class UserLoginActivity : AppCompatActivity() {
 
         }
 
+    }
+
+    private fun observeViewModel() = with(loginViewModel) {
+        onClickLSignInEvent.observe(this@UserLoginActivity) {
+            startActivity(Intent(this@UserLoginActivity, MainActivity::class.java))
+            finish()
+        }
     }
 }
